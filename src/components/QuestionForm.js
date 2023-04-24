4af48a0df9bfde77430adc1d76d18f3902920433
Fares Kerkeni <93195495+Fares-Kerkeni@users.noreply.github.com>
2023-04-24 12:01:02 +0200

    all

---
+++ src/components/QuestionForm.js
@@ -0,0 +1,27 @@
+
+// import { useState } from 'react'
+
+
+// function QuestionForm() {
+//     const [inputValue, setInputValue] = useState("Posez votre question ici")
+//     //si il y a un f
+//     // function checkValue(value) {
+//     //     if (!value.includes('f')) {
+//     //         setInputValue(value)
+//     //     }
+//     // }
+//     return (
+//         <div>
+//             <textarea
+//                 value={inputValue}
+//                 onChange={(e) => setInputValue(e.target.value)}
+//                 // si il y a un f
+//                 // onChange={(e) => checkValue(e.target.value)}
+//             />
+//                 <button onClick={() => alert(inputValue)} onChange={(e) => checkValue(e.target.value)}>Alertez moi 🚨</button>
+
+//         </div> 
+//     )
+// }
+
+// export default QuestionForm
